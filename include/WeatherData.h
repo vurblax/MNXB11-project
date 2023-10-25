e82d8fbb49b754ce215db4e771bb0e677fd4e134
Astrid Lopez <astrid-lopez13@hotmail.com>
2023-10-25 10:41:03 +0200

    Changed Weather Data files

--- include/WeatherData.h
+++ include/WeatherData.h
@@ -1,1 +1,21 @@
-jjj
+#ifndef WEATHERDATA_H
+#define WEATHERDATA_H
+#include <cmath>
+#include <stdexcept>
+
+class WeatherData {
+ public:
+  // Note: Bad idea, using two adjacent parameters that can be switched
+  // accidentally!
+  WeatherData(int year, int month, int day, int hour, int minute, int second, double temperature, int quality);
+
+ private:
+  int year; 
+  int month;
+  int day; 
+  int hour; 
+  int minute; 
+  int second; 
+  double temperature; 
+  int quality;
+};
