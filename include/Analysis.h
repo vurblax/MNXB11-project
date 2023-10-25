f4985f58bfa46780cb01fe5eea443bde5ead9134
Astrid Lopez <astrid-lopez13@hotmail.com>
2023-10-25 15:36:35 +0200

    Analysis.h file completed

--- include/Analysis.h
+++ include/Analysis.h
@@ -1,4 +1,20 @@
-#ifndef ANALYSIS1_H
-#define ANALYSIS1_H
 #ifndef ANALYSIS_H
 #define ANALYSIS_H
+#include <TFile.h>
+#include <TH1D.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "WeatherData.h"
+// Create histograms of the signal and background measurements as well as a
+// histogram representing the sum of the other two and store the results in a
+// ROOT file with name given by output_filename
+void signal_and_background(const std::vector<Measurement>& measurements,
+                           const std::string& output_filename);
+void some_analysis(const std::vector<Measurement>& measurements,
+                   const std::string& output_filename);
+void some_other_analysis(const std::vector<Measurement>& measurements,
+                         const std::string& output_filename);
+#endif /* ANALYSIS_H */
