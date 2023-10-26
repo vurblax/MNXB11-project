e7211ee88454bca4a4a15b5b0e88fc465e4d84b1
Astrid Lopez <astrid-lopez13@hotmail.com>
2023-10-26 22:11:10 +0200

    Tried to plot temperature in x and entries in y

--- src/Analysis.cxx
+++ src/Analysis.cxx
@@ -20,42 +20,31 @@ void CreateDecember25thHistogram(const std::string& output_tempdatafile2) {
         return;
     }
 
-    int year;
     double airtemp;
-    char quality;
-
-    TBranch* yearBranch = dataTree->GetBranch("year");
     TBranch* tempBranch = dataTree->GetBranch("airtemp");
-    TBranch* qualityBranch = dataTree->GetBranch("quality");
-
-    yearBranch->SetAddress(&year);
     tempBranch->SetAddress(&airtemp);
-    qualityBranch->SetAddress(&quality);
 
-    int startYear = 1920;
-    int endYear = 2022;
-    int desiredMonth = 12; // December
-    int desiredDay = 25;   // 25th
+    int minTemp = -20; // Define your desired temperature range
+    int maxTemp = 30;
 
-    TH1D* december25thMeanHist{new TH1D{"december25thMeanHist", "Mean Temperature on Dec 25th",
-        endYear - startYear + 1, startYear - 0.5, endYear + 0.5}};
+    // Define the number of bins based on the temperature range
+    int nBins = maxTemp - minTemp + 1;
+
+    TH1D* december25thHistogram{new TH1D{"december25thHistogram", "Temperature on Dec 25th Histogram",
+        nBins, minTemp - 0.5, maxTemp + 0.5}};
 
     Long64_t nEntries = dataTree->GetEntries();
     for (Long64_t entry = 0; entry < nEntries; ++entry) {
         dataTree->GetEntry(entry);
-        if (year >= startYear && year <= endYear) {
-            // Check if the date is December 25th and the data quality is good
-            if (quality == 'G' && airtemp != -9999 &&
-                static_cast<int>(airtemp) == desiredDay &&
-                static_cast<int>(airtemp / 100) == desiredMonth) {
-                december25thMeanHist->Fill(year, airtemp);
-            }
+        // Check if the date is December 25th and the temperature is within the specified range
+        if (static_cast<int>(airtemp) == 25 && airtemp >= minTemp && airtemp <= maxTemp) {
+            december25thHistogram->Fill(airtemp);
         }
     }
 
-    december25thMeanHist->SetXTitle("Year");
-    december25thMeanHist->SetYTitle("Mean Temperature");
+    december25thHistogram->SetXTitle("Temperature on December 25th");
+    december25thHistogram->SetYTitle("Entries");
 
-    december25thMeanHist->Write();
+    december25thHistogram->Write();
     output_file.Close();
 }
