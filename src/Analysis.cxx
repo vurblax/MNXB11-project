5a56354aa1b137c627c3eb2c8ab6eab64183fa1a
Astrid Lopez <astrid-lopez13@hotmail.com>
2023-10-27 00:04:17 +0200

    Histogram saved into a png file

--- src/Analysis.cxx
+++ src/Analysis.cxx
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <string>
-#include "Analysis.h"
 #include "TFile.h"
-#include "TCanvas.h"
-#include "TH1D.h"
 #include "TTree.h"
-#include <vector>
+#include "TH1D.h"
+#include "TCanvas.h"
+#include "WeatherData.h"
 
 void CreateDecember25thHistogram(const std::string& output_tempdatafile2) {
     TFile output_file{output_tempdatafile2.c_str(), "UPDATE"};
@@ -15,7 +14,7 @@ void CreateDecember25thHistogram(const std::string& output_tempdatafile2) {
         return;
     }
 
-    TTree* dataTree = dynamic_cast<TTree*>(output_file.Get("projectData")); // Replace with the actual tree name
+    TTree* dataTree = dynamic_cast<TTree*>(output_file.Get("projectData"));
     if (!dataTree) {
         std::cerr << "Error: Could not find data tree in the file." << std::endl;
         return;
@@ -37,20 +36,31 @@ void CreateDecember25thHistogram(const std::string& output_tempdatafile2) {
 
     Long64_t nEntries = dataTree->GetEntries();
 
-    for (int year = 1920; year <= 2022; ++year) {
-        for (Long64_t entry = 0; entry < nEntries; ++entry) {
-            dataTree->GetEntry(entry);
-            // Check if the date is December 25th and the year matches
-            if (static_cast<int>(airtemp) == 25 && airtemp >= minTemp && airtemp <= maxTemp &&
-                year == static_cast<int>(airtemp / 10000)) {
-                december25thHistogram->Fill(airtemp);
-            }
+    for (Long64_t entry = 0; entry < nEntries; ++entry) {
+        dataTree->GetEntry(entry);
+        // Check if the date is December 25th
+        int month = static_cast<int>(airtemp / 10000);
+        int day = static_cast<int>(airtemp / 100) % 100;
+        if (month == 12 && day == 25 && airtemp >= minTemp && airtemp <= maxTemp) {
+            december25thHistogram->Fill(airtemp);
         }
     }
 
     december25thHistogram->SetXTitle("Temperature on December 25th");
-    december25thHistogram->SetYTitle("Entries");
+    december25thHistogram->SetYTitle("Frequency");
+
+    TCanvas* c1 = new TCanvas("c1", "December 25th Temperature Histogram", 800, 600);
+    c1->SetFillColor(kGray);
+
+    december25thHistogram->SetLineColor(kRed);
+    december25thHistogram->SetLineWidth(3);
+
+    // Draw with HIST option to create a proper histogram
+    december25thHistogram->Draw("HIST");
+    c1->SaveAs("december25th_histogram.png");
 
     december25thHistogram->Write();
     output_file.Close();
 }
+
+
