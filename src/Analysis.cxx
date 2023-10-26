d239d6a15d4fff0ec8abd41a9a874981100bec5c
Astrid Lopez <astrid-lopez13@hotmail.com>
2023-10-26 17:08:22 +0200

    Changed the Analysis.h file so that it makes sense

--- src/Analysis.cxx
+++ src/Analysis.cxx
@@ -1,33 +1,25 @@
 #include "Analysis.h"
 
-// Function to get temperature data for December 25th in a given year
-double getTemperatureDataForYear(int year) {
-    // Implement your data retrieval logic here, based on your TTree
-    // You need to extract and return the temperature data for December 25th in 'year'
-    // You can use a TTree query or loop through your data as needed
-    // Example: Replace this with your data extraction logic
-    double temperature = 0.0; // Replace with actual data extraction
-    return temperature;
-}
-
 void createTemperatureHistogram() {
-    // Create a ROOT file to save the histogram
-    TFile outputFile("temperature_histogram.root", "RECREATE");
-
     // Define the start and end years for your data
     int startYear = 1780;
     int endYear = 2022;
 
+    // Create a ROOT file to save the histogram
+    TFile outputFile("temperature_histogram.root", "RECREATE");
+
     // Create a histogram to store temperature data
     TH1D* temperatureHistogram = new TH1D("temperature_histogram", "Temperature on December 25th;Year;Temperature (°C)", (endYear - startYear + 1), startYear, endYear + 1);
 
-    // Loop through the years and fill the histogram
     for (int year = startYear; year <= endYear; year++) {
-        // For each year, obtain the temperature data for December 25th
-        double temperature = getTemperatureDataForYear(year);
+        // For each year, obtain a collection of temperature data for the entire year
+        std::vector<WeatherData> yearTemperatureData = getTemperatureDataForYear(year);
 
-        // Fill the histogram with the temperature for December 25th in the current year
-        temperatureHistogram->Fill(year, temperature);
+        // Iterate over the data points for the year and fill the histogram
+        for (const WeatherData& dataPoint : yearTemperatureData) {
+            // Fill the histogram with the temperature data for the current year and data point
+            temperatureHistogram->Fill(year, dataPoint.getTemperature());
+        }
     }
 
     // Set up the histogram properties (labels, colors, etc.) as needed
