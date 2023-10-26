95b46fbfafc4a20c90d36b9ae5faaf3b7d6ddbad
Astrid Lopez <astrid-lopez13@hotmail.com>
2023-10-26 23:20:35 +0200

    Made proper changes to Analysis file to display the correct graph

--- src/Analysis.cxx
+++ src/Analysis.cxx
@@ -5,6 +5,7 @@
 #include "TCanvas.h"
 #include "TH1D.h"
 #include "TTree.h"
+#include <vector>
 
 void CreateDecember25thHistogram(const std::string& output_tempdatafile2) {
     TFile output_file{output_tempdatafile2.c_str(), "UPDATE"};
@@ -24,21 +25,26 @@ void CreateDecember25thHistogram(const std::string& output_tempdatafile2) {
     TBranch* tempBranch = dataTree->GetBranch("airtemp");
     tempBranch->SetAddress(&airtemp);
 
-    int minTemp = -20; // Define your desired temperature range
+    int minTemp = -10; // Define your desired temperature range
     int maxTemp = 30;
 
     // Define the number of bins based on the temperature range
     int nBins = maxTemp - minTemp + 1;
 
-    TH1D* december25thHistogram{new TH1D{"december25thHistogram", "Temperature on Dec 25th Histogram",
+    // Create a histogram to count the occurrences of each temperature
+    TH1D* december25thHistogram{new TH1D{"december25thHistogram", "Temperature on Dec 25th",
         nBins, minTemp - 0.5, maxTemp + 0.5}};
 
     Long64_t nEntries = dataTree->GetEntries();
-    for (Long64_t entry = 0; entry < nEntries; ++entry) {
-        dataTree->GetEntry(entry);
-        // Check if the date is December 25th and the temperature is within the specified range
-        if (static_cast<int>(airtemp) == 25 && airtemp >= minTemp && airtemp <= maxTemp) {
-            december25thHistogram->Fill(airtemp);
+
+    for (int year = 1920; year <= 2022; ++year) {
+        for (Long64_t entry = 0; entry < nEntries; ++entry) {
+            dataTree->GetEntry(entry);
+            // Check if the date is December 25th and the year matches
+            if (static_cast<int>(airtemp) == 25 && airtemp >= minTemp && airtemp <= maxTemp &&
+                year == static_cast<int>(airtemp / 10000)) {
+                december25thHistogram->Fill(airtemp);
+            }
         }
     }
 
