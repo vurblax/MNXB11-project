f010b79971c742c29157b0ebb3dbd2731d1cb4be
Astrid Lopez <astrid-lopez13@hotmail.com>
2023-10-26 17:41:43 +0200

    Changes in Analysis.h

--- include/Analysis.h
+++ include/Analysis.h
@@ -12,10 +12,7 @@
 #include "WeatherData.h"
 
 
-// Function to get temperature data for December 25th in a given year
-double getTemperatureDataForYear(const std::vector<WeatherData>& WeatherData, const std::string& TempYearHist);
-
-void createTemperatureHistogram();
+void createTemperatureHistogram(const std::string& output_tempdatafile2);
 
 
 #endif /* ANALYSIS_H */
