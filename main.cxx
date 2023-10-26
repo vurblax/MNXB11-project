854157059f7ab553c18e3c9221a25e9d4703ef5f
Marta <martvi07@ucm.es>
2023-10-26 17:33:00 +0200

    changed Makefile and main.cxx

--- main.cxx
+++ main.cxx
@@ -6,7 +6,7 @@
 #include "date.h"
 #include "csv.h"
 #include "Analysis_2.h"
-//#include "Analysis_3.h"
+#include "Analysis_3.h"
 
 int main() {
     // DATA EXTRACTION PART
@@ -40,9 +40,9 @@ LongtermTemp(output_tempdatafile);
 // more analyses to be added here
 
 
- // std::vector<Measurement> measurements = load_measurements("your_data.csv"); // Replace with your data file
+  std::vector<Measurement> measurements = load_measurements("your_data.csv"); // Replace with your data file
 
-  //analyze_warmest_and_coldest_days(measurements, "output.root");
+  analyze_warmest_and_coldest_days(measurements, "output.root");
 
   return 0;
 }
